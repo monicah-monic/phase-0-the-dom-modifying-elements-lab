6cea06a45dd1c2e2ac3499926738056e1f8c731d
monicah-monic <monicahwanjiru205@gmail.com>
2025-03-27 12:04:20 +0300

    modifying dom

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Write your code here!
+
+document.querySelector("main").remove();
+
+let newHeader = document.createElement("h1");
+
+newHeader.id = "victory";
+newHeader.textContent = "LARRY KIPKURUI  is the champion";
+
+document.body.append(newHeader);
